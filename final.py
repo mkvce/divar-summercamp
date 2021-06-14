4b1357712b2cff9e18c947172ec2496aac10c192
Mahdi <mahdi57.kamali1@gmail.com>
2021-06-14 11:19:44 +0430

    Complete section 2

--- final.py
+++ final.py
@@ -19,16 +19,22 @@ class Desk:
         return False
 
 
-class Floor:
-    def __init__(self, number: int, num_of_desks=0):
-        self.__id = number
-        self.__desks = [Desk(i, self.__id) for i in range(1, num_of_desks + 1)]
 
+class Floor:
+    def __init__(self, number: int, floor_type, price, num_of_desks=0):
+        self.__id = number
+        self.__desks = [Desk(i, self.__id, price) for i in range(1, num_of_desks + 1)]
+        self.__type = floor_type
+        self.__price = price
+    
+    @property
+    def type(self):
+        return self.__type
 
     def get_desk(self, ts: int, duration: int):
         for desk in self.__desks:
             if desk.reserve(ts, duration):
-                return desk.id
+                return (desk.id, self.__price)
         raise NoDeskException()
 
     def add_desk(self):
@@ -36,28 +42,38 @@ class Floor:
 
     
 class WorkSpaceManager:
-    def __init__(self, num_of_floors: int):
+    def __init__(self, num_of_floors=0):
         self.__floors = [Floor(i) for i in range(1, num_of_floors + 1)]
 
     def add_desk(self, floor: int, num=1):
         for _ in range(num):
             self.__floors[floor - 1].add_desk()
 
-    def get_desk(self, ts: int, username: str, duration: int):
+    def get_desk(self, ts: int, username: str, floor_type: str, duration: int):
         for floor in self.__floors:
-            try:
-                return floor.get_desk(ts, duration)
-            except NoDeskException:
-                continue
+            if floor.type == floor_type:
+                try:
+                    return floor.get_desk(ts, duration)
+                except NoDeskException:
+                    continue
         raise NoDeskException()
+
+    def add_floor(self, type, price):
+        self.__floors.append(Floor(len(self.__floors) + 1, type, price))
     
 
 class UserInterface:
     def __init__(self):
-        num_of_floors = int(input())
-        self.__manager = WorkSpaceManager(num_of_floors)
+        num_of_floors, special_floor_price = [int(x) for x in input().split()]
+        self.__manager = WorkSpaceManager()
         for floor in range(1, num_of_floors + 1):
-            num_of_desks = int(input())
+            num_of_desks, floor_type = input().split()
+            num_of_desks = int(num_of_desks)
+            if floor_type == 'free':
+                price = 0
+            elif floor_type == 'special':
+                price = special_floor_price
+            self.__manager.add_floor(floor_type, price)
             self.__manager.add_desk(floor, num_of_desks)
         self.__run()
         
@@ -73,9 +89,13 @@ class UserInterface:
                 ts = int(cmd[0])
                 if cmd[1] == 'request_desk':
                     username = cmd[2]
-                    duration = int(cmd[3])
-                    desk_id = self.__manager.get_desk(ts, username, duration)
-                    print(f"{username} got desk {desk_id}")
+                    floor_type = cmd[3]
+                    duration = int(cmd[4])
+                    desk_id, price = self.__manager.get_desk(ts, username, floor_type, duration)
+                    if price:
+                        print(f"{username} got desk {desk_id} for {price}")
+                    else:
+                        print(f"{username} got desk {desk_id}")
             except NoDeskException as err:
                 print('no desk available')
     
